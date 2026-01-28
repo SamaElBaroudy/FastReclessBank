27f5e920c518bf9698b7735b5e0e97e4151e4111
Sama Elbaroudy <samaelbaroudy9@gmail.com>
2026-01-28 14:01:14 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/fastrecklessbank/bank/Revel8BankApplication.java
@@ -0,0 +1,13 @@
+package com.fastrecklessbank.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Revel8BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Revel8BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastrecklessbank/bank/Revel8BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastrecklessbank.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Revel8BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
